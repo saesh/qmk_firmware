fb4d4aeeb0fc63c6e261a522d00e58edb5bc587e
Sascha Hagedorn <mail@shagedorn.net>
2019-04-24 21:31:43 +0200

    Add saesh ok60 keymap

---
+++ keyboards/ok60/keymaps/saesh/rules.mk
@@ -0,0 +1,6 @@
+BOOTMAGIC_ENABLE = full
+MOUSEKEY_ENABLE = no
+COMMAND_ENABLE = no # disabled for space cadet feature, see https://beta.docs.qmk.fm/features/feature_space_cadet_shift#caveats
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+TAP_DANCE_ENABLE = yes
