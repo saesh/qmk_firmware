adfe4076ab7b755610fe68f8733422c32cd90113
Sascha Hagedorn <mail@shagedorn.net>
2019-08-17 18:27:01 +0200

    Redo keyboard layout

--- keyboards/crkbd/keymaps/saesh/config.h
+++ keyboards/crkbd/keymaps/saesh/config.h
@@ -28,20 +28,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define SSD1306OLED
+//#define SSD1306OLED
 
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+    #define RGBLED_NUM 27
+
+    #define RGBLIGHT_SLEEP
+    // #define RGBLIGHT_SPLIT
+
+    // #define RGBLIGHT_ANIMATIONS  // disable all animations
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
 
 #undef PRODUCT
 #define PRODUCT Corne Keyboard - saesh configuration
