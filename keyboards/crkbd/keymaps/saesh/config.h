e5a789b70cb209caba1b9510baeb667e7c2aa60c
Sascha Hagedorn <mail@shagedorn.net>
2019-09-19 23:07:55 +0200

    Change default RGB mode

--- keyboards/crkbd/keymaps/saesh/config.h
+++ keyboards/crkbd/keymaps/saesh/config.h
@@ -67,7 +67,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_MATRIX_VAL_STEP 5
 #    define RGB_MATRIX_SPD_STEP 10
 
-#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_GRADIENT_UP_DOWN
 #endif
 
 #undef PRODUCT
