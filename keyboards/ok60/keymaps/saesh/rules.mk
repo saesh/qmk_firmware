959c6e015a994483444cf17039b22b21391d6dd9
Sascha Hagedorn <mail@shagedorn.net>
2019-09-26 16:06:33 +0200

    Use hjkl only for navigation

--- keyboards/ok60/keymaps/saesh/rules.mk
+++ keyboards/ok60/keymaps/saesh/rules.mk
@@ -1,6 +1,6 @@
 BOOTMAGIC_ENABLE = full
-MOUSEKEY_ENABLE = no
-COMMAND_ENABLE = no # disabled for space cadet feature, see https://beta.docs.qmk.fm/features/feature_space_cadet_shift#caveats
+MOUSEKEY_ENABLE  = no
+COMMAND_ENABLE   = no # disabled for space cadet feature, see https://beta.docs.qmk.fm/features/feature_space_cadet_shift#caveats
 BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = no
+RGBLIGHT_ENABLE  = no
 TAP_DANCE_ENABLE = yes
