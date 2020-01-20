bb0645c1f707b116aa702310177a623941411807
Sascha Hagedorn <mail@shagedorn.net>
2020-01-20 18:26:04 +0100

    Fix red LEDs not off after flash

--- keyboards/kingly_keys/romac/keymaps/saesh/keymap.c
+++ keyboards/kingly_keys/romac/keymaps/saesh/keymap.c
@@ -94,6 +94,14 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
   return true;
 };
 
+void matrix_init_kb(void) {
+   DDRD &= ~(1<<5);
+   PORTD &= ~(1<<5);
+
+   DDRB &= ~(1<<0);
+   PORTB &= ~(1<<0);
+}
+
 void matrix_init_user(void) {
   rgblight_setrgb(0xff, 0x80, 0x00);
 };
