85536ca7156d240a5333f42e7df6f92d2cc36686
Sascha Hagedorn <mail@shagedorn.net>
2020-12-23 11:30:15 +0100

    [kbd19x] caps is ctrl

--- keyboards/kbdfans/kbd19x/keymaps/saesh/keymap.c
+++ keyboards/kbdfans/kbd19x/keymaps/saesh/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RGB_TOG,                   KC_HOME, KC_END,  KC_PGUP, KC_PGDN, \
     DE_CIRC, DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, _______, KC_BSPC,          KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
     KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UDIA, DE_PLUS, DE_HASH,                   KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-    KC_CAPS, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA,                    KC_ENT,          KC_P4,   KC_P5,   KC_P6,   _______, \
+    KC_LCTL, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA,                    KC_ENT,          KC_P4,   KC_P5,   KC_P6,   _______, \
     KC_LSFT, DE_LABK, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS,     KC_RSFT,   KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_PDOT, _______           \
   ),
