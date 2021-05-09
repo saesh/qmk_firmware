4142cbbcfc1bb2b8686c4e6f3e41fab5ae067d76
Sascha Hagedorn <mail@shagedorn.net>
2021-05-09 21:17:29 +0200

    [gothic70] update keymap

--- keyboards/gothic70/keymaps/saesh/keymap.c
+++ keyboards/gothic70/keymaps/saesh/keymap.c
@@ -1,38 +1,33 @@
 #include QMK_KEYBOARD_H
-
 #include "saesh.h"
 
-// Tap Dance Declarations
-enum {
-  TD_SCAPS = 0
-};
-
-#define SCAPS_LG TD(TD_SCAPS)
-#define IJ_CMD LGUI(LSFT(KC_A))
+#define KC_SEC1 KC_SECRET_1
+#define KC_SEC2 KC_SECRET_2
+#define KC_SEC3 KC_SECRET_3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
      KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_GRV,          KC_HOME, \
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,           KC_INS, \
-    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,                    KC_END, \
-   SCAPS_LG,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MO(2),    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,          \
+    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,           KC_END, \
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MO(2),    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,          \
     KC_LCTL, KC_LGUI, KC_LALT,           KC_SPC,   MO(1),          KC_BSPC,          KC_RALT,                   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   [1] = LAYOUT(
-    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, _______,           IJ_CMD, \
+    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,          _______, \
     _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,  KC_DEL,          _______, \
-    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,          KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,          _______, \
-    _______, RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_PGUP,          \
-    _______, _______, _______,          _______, _______,           KC_DEL,          _______,                   _______,          KC_HOME, KC_PGDN,  KC_END  \
+    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,          _______,          _______, \
+    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______,          _______,          _______,          \
+    _______, _______, _______,          _______, _______,           KC_DEL,          _______,                   _______,          _______, _______, _______  \
   ),
 
   [2] = LAYOUT(
-    KC_SLEP, KC_SECRET_1, KC_SECRET_2, KC_SECRET_3, KC_SECRET_4, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         KC_MAKE, \
-    _______, _______, _______, _______,   RESET, _______,          _______, _______, _______, _______, _______, _______, _______, _______,         _______, \
-    _______, AG_SWAP, AG_NORM, _______, _______, _______,          _______, _______, _______, _______, _______, _______,          KC_MPLY,         _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_VOLU,         \
-    _______, _______, _______,          _______, _______,          _______,          _______,                   _______,          KC_MPRV, KC_VOLD, KC_MNXT \
+    KC_SLEP, KC_SEC1, KC_SEC2, KC_SEC3, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+    _______, _______, _______, _______,   RESET, _______,          _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,          _______,          _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MAKE, _______, _______, _______,          _______,          _______,          \
+    _______, _______, _______,          _______, _______,          _______,          _______,                   _______,          _______, _______, _______  \
   )
 };
 
@@ -71,9 +66,3 @@ void led_set_keymap(uint8_t usb_led) {
     writePinLow(F7);
   }
 }
-
-// Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-  // double tap for caps
-  [TD_SCAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-};
