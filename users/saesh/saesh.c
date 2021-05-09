bc602caac75bd7491d2dff8027e49639b3614417
Sascha Hagedorn <mail@shagedorn.net>
2021-05-09 21:30:14 +0200

    double q is i3 quit sequence

--- users/saesh/saesh.c
+++ users/saesh/saesh.c
@@ -88,4 +88,5 @@ void tap_dance_esc_grave_tilde(qk_tap_dance_state_t *state, void *user_data) {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_GRAVE] = ACTION_TAP_DANCE_FN(tap_dance_esc_grave_tilde),
+  [TD_QUIT] = ACTION_TAP_DANCE_DOUBLE(KC_Q, SGUI(KC_Q)),
 };
