a034c788a948d268988d9ab39c0ef778c7a4e72c
Sascha Hagedorn <mail@shagedorn.net>
2021-05-13 08:07:29 +0200

    [gothic70] Add modifiers to home row

--- keyboards/gothic70/keymaps/saesh/keymap.c
+++ keyboards/gothic70/keymaps/saesh/keymap.c
@@ -7,26 +7,26 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_GRV,           KC_DEL,  \
-     KC_TAB,    KC_QUIT, KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,           KC_PGUP, \
-    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,           KC_PGDN, \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MO(2),    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,           \
-    KC_LCTL, KC_LGUI, KC_LALT,           KC_SPC,   MO(1),          KC_BSPC,          KC_RALT,                   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT   \
+     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_GRV,           KC_DEL, \
+     KC_TAB,    KC_QUIT, KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGUP, \
+    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,          KC_PGDN, \
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   MO(2),    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,          \
+    KC_LCTL, KC_LGUI, KC_LALT,           KC_SPC,   MO(1),          KC_BSPC,          KC_RALT,                   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   [1] = LAYOUT(
-    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,        G(C(KC_M)), \
-    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,        G(C(KC_A)), \
-    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,        G(S(KC_F)), \
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_HOME,  KC_END, KC_PGUP, KC_PGDN, _______,          _______,          _______,          \
+    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,       G(C(KC_M)), \
+    _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,       G(C(KC_A)), \
+    _______, _______, _______, KC_LALT, KC_LSFT, KC_LCTL,          KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,       G(S(KC_F)), \
+    _______, _______, _______, _______, _______, _______, _______, KC_HOME,  KC_END, KC_PGUP, KC_PGDN, _______,          _______,          _______,          \
     _______, _______, _______,          _______, _______,           KC_DEL,          _______,                   _______,          _______, _______, _______  \
   ),
 
   [2] = LAYOUT(
     KC_SLEP, KC_SEC1, KC_SEC2, KC_SEC3, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
     _______, _______, _______, _______,   RESET, _______,          RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______,          _______, \
-    _______, _______, _______, _______, _______, _______,          RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______,          _______,          _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MAKE, _______, _______, _______,          _______,          _______,          \
+    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______,          _______,          _______, \
+    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, KC_MAKE, _______, _______, _______,          _______,          _______,          \
     _______, _______, _______,          _______, _______,          _______,          _______,                   _______,          _______, _______, _______  \
   )
 };
